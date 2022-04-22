0d76355798688c20fcce1c50b708b8d98a8982db
Johnsonkdavid <36987064+Johnsonkdavid@users.noreply.github.com>
2022-04-22 14:22:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,3 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
-terraform {
-  required_providers {
-    mycloud = {
-      source  = "hashicorp/aws"
-      version = ">= 1.0"
-    }
-  }
-}
-
